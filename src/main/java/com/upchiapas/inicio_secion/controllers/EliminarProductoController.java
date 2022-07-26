bdf3e4e235f74227184da22f04428ea3e9065a31
practica <velazquezhernandezjesus13@gmail.com>
2022-07-26 18:26:23 -0500

    Se implemento la retrincion de no poder ingresar mas que la cantidad que se tiene

--- src/main/java/com/upchiapas/inicio_secion/controllers/EliminarProductoController.java
+++ src/main/java/com/upchiapas/inicio_secion/controllers/EliminarProductoController.java
@@ -24,26 +24,41 @@ public class EliminarProductoController {
     @FXML
     void btnEliminarProducto(MouseEvent event) {
         Iterator<Producto> iterator = AgregarProductoController.getProductos().iterator();
-        int id, i=0, cantidad, respaldo;
+        int id, i=-1, cantidad, respaldo;
         try {//validacion de la id y cantidad que sea un número
             id=Integer.parseInt(txtId.getText());
             cantidad = Integer.parseInt(txtCantidad.getText());
-            boolean bantera = false;
-            while (!bantera && iterator.hasNext()) {
-                if (iterator.next().getId() == id) {
-                    respaldo = AgregarProductoController.getProductos().get(i).getCantidad() - cantidad;
-                    AgregarProductoController.getProductos().get(i).setCantidad(respaldo);
-                    FastTrackingApplication.setFXML("Home-view", "Fast Tracking");
-                    bantera = true;
-                }
+            boolean bantera = false, encontrado=false;
+            while (!encontrado && iterator.hasNext()) {
                 i++;
+                if (iterator.next().getId() == id) {
+                    if(cantidad<=AgregarProductoController.getProductos().get(i).getCantidad()) {
+                        respaldo = AgregarProductoController.getProductos().get(i).getCantidad() - cantidad;
+                        AgregarProductoController.getProductos().get(i).setCantidad(respaldo);
+                        bantera = true;
+                    }
+                    else {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setHeaderText(null);
+                        alert.setTitle("Error");
+                        alert.setContentText("La cantidad a eliminar es mas que la actual");
+                        alert.showAndWait();
+                    }
+                    encontrado=true;
+                }
             }
-            if (!bantera) {
+            if (!encontrado) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setHeaderText(null);
-                alert.setTitle("Producto - Error");
+                alert.setTitle("Error");
                 alert.setContentText("Producto no encontrado");
                 alert.showAndWait();
+            }
+            if (AgregarProductoController.getProductos().get(i).getCantidad()==0){
+                AgregarProductoController.getProductos().remove(i);
+                FastTrackingApplication.setFXML("Home-view", "Fast Tracking");
+            }else if(bantera) {
+                FastTrackingApplication.setFXML("Home-view", "Fast Tracking");
             }
         }catch (NumberFormatException e){//muestra al usuario su error
             Alert alert = new Alert(Alert.AlertType.ERROR);
